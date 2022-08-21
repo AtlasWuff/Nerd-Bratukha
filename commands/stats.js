b321b908fd77dda3041fa23abdecd519f8e8d6e6
Its-Tachanks <joshuaew06@gmail.com>
2022-08-21 19:19:31 -0400

    Create stats.js

---
+++ commands/stats.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require('discord.js');
+const fs = require('fs');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+		.setName('stats')
+		.setDescription('View the server and member stats!'),
+
+    async execute(client, interaction) {
+        interaction.reply({
+            embeds: [{
+                title: 'Bot Stats',
+                color: 12717312,
+                fields: [
+                    {
+                        name: "Servers",
+                        value: client.guilds.cache.size
+                    },
+                    {
+                        name: "Users",
+                        value: client.users.cache.size
+                    }
+                ]
+            }],
+        });
+    },
+};
